cb4c46a81b17b28e329d013552195af95c67105a
seungh0 <will.seungho@gmail.com>
2020-06-18 17:47:36 +0900

    Initial Commit

---
+++ src/main/java/me/will/App.java
@@ -0,0 +1,10 @@
+package me.will;
+
+public class App {
+
+	public static void main(String[] args) {
+//		Moja moja = new MojaFactory();
+//		System.out.println(moja.pullOut());
+	}
+
+}
---
+++ src/main/java/me/will/Moja.java
@@ -0,0 +1,8 @@
+package me.will;
+
+@Magic
+public interface Moja {
+
+	String pullOut();
+
+}
