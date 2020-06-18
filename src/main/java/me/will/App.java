19c2d9de91cf72ba85694af44f3e66aeabba1ac2
seungh0 <will.seungho@gmail.com>
2020-06-18 18:07:29 +0900

    Use MagicFactory (made by Annotation Processor)

--- src/main/java/me/will/App.java
+++ src/main/java/me/will/App.java
@@ -3,8 +3,8 @@ package me.will;
 public class App {
 
 	public static void main(String[] args) {
-//		Moja moja = new MojaFactory();
-//		System.out.println(moja.pullOut());
+		Moja moja = new MagicFactory();
+		System.out.println(moja.pullOut());
 	}
 
 }
---
+++ src/main/java/me/will/MyMoja.java
@@ -0,0 +1,9 @@
+package me.will;
+
+/**
+ * Annotation Processor 정의시 인터페이스만 사용하게 하였으므로,
+ * 클래스에서 @Moja 사용시 컴파일 에러 발생!
+ */
+//@Magic
+public class MyMoja {
+}
